5535b7c473b4b6232dab0db5deb2ed2b53bcfb31
Shubham Verma <Shubham Verma@DESKTOP-KVV1592>
2019-01-17 01:22:58 +0530

    I am adding program of Handle Table Grid Selenium scenario.....

---
+++ SeleniumScenarioHandling/src/handlingTableGrids/HandleTableGrid.java
@@ -0,0 +1,30 @@
+package handlingTableGrids;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleTableGrid {
+
+	public static WebDriver driver;
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("https://www.cricbuzz.com/live-cricket-scorecard/21587/syl-vs-rgr-18th-match-bangladesh-premier-league-2019");
+		driver.manage().window().maximize();
+		
+		WebElement table =driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
+		
+		int rowCount=table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms']"))
+		.size();
+		int count=table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)"))
+		.size();
+		
+		for(int i=0;i<count-2;i++){
+
+			System.out.println(table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).get(i).getText());
+		}
+	}
+}
